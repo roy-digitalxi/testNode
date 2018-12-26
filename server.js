e3638b316afb8d2ce0c10ca032ca2d0c9f72c4cb
roy <roy@digitalxi.com>
2018-12-26 13:18:49 -0500

    test node

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.get('/', function (req, res) {
     }
     return res.json({
       confirmation: 'success 123',
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
     })
   })
 
