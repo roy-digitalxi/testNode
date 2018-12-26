9222a5d8e986b1ecc7022176b0c9504bf1799d01
roy <roy@digitalxi.com>
2018-12-26 12:28:43 -0500

    test

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const express = require('express');
+const mysql  = require('mysql');
+
+// Constants
+const PORT = 3000;
+
+
+// App
+const app = express();
+app.get('/test_con', function (req, res) {
+
+  //Connect to DB
+  const mysqlCon = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+  });
+
+  mysqlCon.connect((err) => {
+    if(err) {
+      return res.json({
+        confirmation: 'fail',
+        message: err
+      })
+    }
+    return res.json({
+      confirmation: 'success',
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+    })
+  })
+
+});
+
+app.listen(PORT);
+
