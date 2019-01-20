84133538f4675fb69f529b628b73861aefd833c9
roy <roy@digitalxi.com>
2019-01-19 20:55:20 -0500

    test

--- routes/upload/test/test.js
+++
@@ -1,6 +0,0 @@
-import middlewares from "../../../middlewares";
-import * as emailHelper from "../../../utilities/emailHelper";
-
-const Controller = {};
-
-export default Controller;
--- utilities/token.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import constants from "../constants";
-
-export const createToken = UserGUID => {
-  return jwt.sign({ UserGUID: UserGUID }, constants.JWT_SECRET, {
-    expiresIn: 3600
-  });
-};
