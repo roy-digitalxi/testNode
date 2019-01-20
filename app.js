eaac673a48db46949ca008208b581e48998472c8
roy <roy@digitalxi.com>
2019-01-19 19:22:42 -0500

    test node

--- app.js
+++ app.js
@@ -17,31 +17,6 @@ const qs = require('qs');
 // user: root
 // pwd: password
 
-
-// keycloak
-const adminClient = require('keycloak-admin-client');
-const KeycloakMultirealm = require('./keycloak-connect-multirealm');
-const session = require('express-session');
-
-
-// DB
-const mysql = require('mysql');
-const MongoClient = require('mongodb').MongoClient;
-const mongoose = require('mongoose');
-
-
-const mysqlHost = 'mysql-bitnami-mysql.default.svc.cluster.local';
-const mysqlUser = 'root';
-const mysqlPassword = 'root';
-
-const mongoDbHost = 'mongodb-bitnami.default.svc.cluster.local';
-const mongoDbUser = 'root';
-const mongoDbPassword = 'password';
-
-const keycloakHost = 'http://35.203.121.29/auth';
-const keycloakUser = 'keycloak';
-const keycloakPassword = '3NkxhxutiN';
-
 // express
 const app = express();
 const port = (process.env.PORT || 3000);
